1da8f2c96cc171f3c907e1b9bdd87d72b0e559ae
Rui <rui.ferreira.v@gmail.com>
2024-07-05 16:31:39 -0300

    Adicionado endpoint para adicionar itens ao carrinho

--- app.py
+++ app.py
@@ -5,7 +5,7 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "testprog"
@@ -130,8 +130,23 @@ def get_products():
         product_list.append(product_data)
     return jsonify(product_list)
 
+# Adicionado endpoint para adicionar itens ao carrinho
+# Chekout
+@app.route("/api/cart/add/<int:product_id>", methods = ["POST"])
+@login_required
+def add_to_cart(product_id):
+    # Usuário
+    user = User.query.get(int(current_user.id))
+    # Produto
+    product = Product.query.get(product_id)
 
-
+    if user and product:
+        cart_item = CartItem(user_id = user.id, product_id = product.id)
+        db.session.add(cart_item)
+        db.session.commit()
+        return jsonify({"message":"Item added to the cart successfully"})
+    return jsonify({"message":"Failed to add item to the cart"}), 400
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
